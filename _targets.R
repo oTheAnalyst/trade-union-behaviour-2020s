e33d6bc34f84ed75b642ca68dd20f779395a914c
VtheRtech <sakeyisu@gmail.com>
2024-04-24 00:03:46 -0400

    created a robust pipeline

--- _targets.R
+++ _targets.R
@@ -1,33 +1,53 @@
 library(targets)
 library(tarchetypes)
 
+
 source("~/Lab4/functions/functions.R")
 options(clustermq.scheduler = "multicore")
 
 tar_option_set(
   packages = c(
-    "tibble", "dplyr", "stringr",
-    "ggplot2", "purrr", "magrittr", "DBI", "RSQLite"
+    "tibble", "dplyr", "stringr", "readr",
+    "ggplot2", "purrr", "magrittr", "DBI", "RSQLite", "forcats"
   )
 )
 
 
-dmv <- c("Maryland","Virginia","Disctrict of Columbia")
+dmv <- c("Maryland", "Virginia", "District of Columbia")
 
 list(
   tar_target(
     data,
     load_data(data = "LAT-02.19.24"),
-    format = "qs"
+    format = "feather"
   ),
   tar_target(
     transformed_data,
     transform_data(data = data),
-    format = "qs"),
+    format = "feather"
+  ),
+  tar_target(
+    strikes_maryland,
+    number_of(state_var = "Maryland", transformed_data),
+    format = "fst"
+  ),
+  tar_target(
+    strikes_virginia,
+    number_of(state_var = "Virginia", transformed_data),
+    format = "fst"
+  ),
+  tar_target(
+    strikes_dc,
+    number_of(state_var = "District of Columbia", transformed_data),
+    format = "fst"
+  ),
   tar_target(
     plot_states,
     plot_data(state_var = dmv, transformed_data),
-    format = "qs"
+    format = "fst"
+  ),
+  tar_target(
+    plot_time_series_virginia,
+    plot_time_series(data = strikes_virginia, state = "Virginia"),
+    format = "fst")
   )
-)
-
--- functions/functions.R
+++ functions/functions.R
@@ -7,23 +7,23 @@ load_data <- function(data) {
 }
 
 transform_data <- function(data) {
-# Convert Timestamp column to POSIXct format
-data$Timestamp <- as.POSIXct(data$Timestamp,
-  format = "%m/%d/%Y %H:%M:%S"
-)
-# mutate zip code to a character
-data <- data %>%
-  mutate(
-    ZipCode = as.character(ZipCode),
-    BargainingUnitSize = parse_number(as.character(BargainingUnitSize)),
-    ApproximateNumberofParticipants =
-      parse_number(as.character(ApproximateNumberofParticipants)),
-    Date = format(Timestamp, "%Y-%m-%d"),
-    month = format(Timestamp, "%B"),
-    Year = format(Timestamp, "%Y"),
-    DurationAmount = as.integer(DurationAmount)
+  # Convert Timestamp column to POSIXct format
+  data$Timestamp <- as.POSIXct(data$Timestamp,
+    format = "%m/%d/%Y %H:%M:%S"
   )
-return(data)
+  # mutate zip code to a character
+  data <- data %>%
+    mutate(
+      ZipCode = as.character(ZipCode),
+      BargainingUnitSize = parse_number(as.character(BargainingUnitSize)),
+      ApproximateNumberofParticipants =
+        parse_number(as.character(ApproximateNumberofParticipants)),
+      Date = format(Timestamp, "%Y-%m-%d"),
+      month = format(Timestamp, "%B"),
+      Year = format(Timestamp, "%Y"),
+      DurationAmount = as.integer(DurationAmount)
+    )
+  return(data)
 }
 
 
@@ -31,25 +31,57 @@ plot_data <- function(state_var, transformed_data) {
   years <- unique(transformed_data$Year)
   if (length(years) == 1) {
     d <- transformed_data %>%
-      filter(State == state_var, Year == years) %>%
-      ggplot(aes(x = month)) +
+      filter(State == state_var & Year == years) %>%
+      ggplot(aes(x = month))
       geom_bar() +
-      labs(title = paste("Number of Strikes in", state_var)) +
-      common_theme()
+      labs(title = paste("Number of Strikes in", state_var))
   } else {
     d <- transformed_data %>%
-      filter(State == state_var, Year %in% years) %>%
+      filter(State == state_var & Year %in% years) %>%
       ggplot(aes(x = month)) +
       geom_bar() +
       labs(title = paste("Number of Strikes in", state_var, "Per Month")) +
-      facet_wrap(~Year, scales = "free_x") +
-      common_theme()
+      facet_wrap(~Year, scales = "free_x")
   }
   # Save the plot as a file
   plot_filename <- paste0(state_var, "_plot.png")
   ggsave(plot_filename, plot = d, width = 10, height = 6, dpi = 300)
-  return(plot_filename)  # Return the filename for downstream targets
+  plot_filename # Return the filename for downstream targets
 }
 
 
+number_of <- function(state_var, transformed_data) {
+  if (!is.vector(state_var)) {
+    state_var <- as.vector(state_var)
+  }
+  transformed_data %>%
+    filter(State == state_var) %>%
+    group_by(State, Year) %>%
+    summarise(
+      labor_org_count = n_distinct(LaborOrganization),
+      strikes = n(),
+      .groups = "drop"
+    )
+}
+
+plot_time_series <- function(data, state) {
+  # Filter the data for the specified state
+  state_data <- data[data$State == state, ]
+
+  # Convert Year to a date format for time series plotting
+  state_data$Year <- as.Date(paste0(state_data$Year, "-01-01"))
+
+  # Create the dual-axis line chart
+  p <- ggplot(state_data, aes(x = Year)) +
+    geom_line(aes(y = strikes, color = "Strikes"), size = 1.5) +
+    geom_line(aes(y = labor_org_count * 10, color = "Labor Org Count"), linetype = "dashed", size = 1.5) +
+    scale_color_manual(values = c("Strikes" = "blue", "Labor Org Count" = "red"),
+                       labels = c("Strikes", "Labor Org Count")) +
+    labs(title = paste("Strikes and Labor Organization Count Over Time in", state),
+         x = "Year",
+         y = "Count") +
+    theme_minimal()
+
+  return(p)
+}
 
