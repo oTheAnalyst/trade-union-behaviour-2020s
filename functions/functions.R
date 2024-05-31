e1f757e2d8e0a9529f0451fe17dc20553aef2ca2
VtheRtech <sakeyisu@gmail.com>
2024-05-30 20:26:58 -0400

    fixed global variables warnings, made code more verbos

--- functions/functions.R
+++ functions/functions.R
@@ -8,30 +8,34 @@ load_and_transform_data <- function(index) {
   # Function to read Excel file, convert to tibble, and transform data
   read_and_transform <- function(file_path) {
     # Read Excel file and convert to tibble
-    excel_tibble <- read_excel(file_path)
+    excel_tibble <- readxl::read_excel(file_path)
     excel_tibble$Timestamp <- as.POSIXct(excel_tibble$Timestamp,
       format = "%m/%d/%Y %H:%M:%S", tz = "UTC"
     )
-    excel_tibble$Year <- year(excel_tibble$Timestamp)
-    excel_tibble$Month <- month(excel_tibble$Timestamp)
+    excel_tibble$Year <- lubridate::year(excel_tibble$Timestamp)
+    excel_tibble$Month <- lubridate::month(excel_tibble$Timestamp)
     return(excel_tibble)
     # Mutate columns as needed
-    transformed_data <- excel_tibble %>%
-      mutate(
-        ZipCode = as.character(Zip.Code),
+    transformed_data <- excel_tibble |>
+      dplyr::mutate(
+        ZipCode = as.character(transformed_data::Zip.Code),
         BargainingUnitSize =
-          readr::parse_number(as.character(Bargaining.Unit.Size)),
+          readr::parse_number(as.character(
+            transformed_data::Bargaining.Unit.Size
+          )),
         ApproximateNumberofParticipants =
-          readr::parse_number(as.character(Approximate.Number.of.Participants)),
-        Date = format(Timestamp, "%m-%d-%Y"),
-        month = format(Timestamp, "%B"),
-        DurationAmount = as.integer(Duration.Amount)
+          readr::parse_number(as.character(
+            transformed_data::Approximate.Number.of.Participants
+          )),
+        Date = format(transformed_data::Timestamp, "%m-%d-%Y"),
+        month = format(transformed_data::Timestamp, "%B"),
+        DurationAmount = as.integer(transformed_data::Duration.Amount)
       )
     return(transformed_data)
   }
   # Read all Excel files, store them as tibbles, and transform data
-  transformed_data_list <- map(file_paths, read_and_transform)
-  transformed_ <- as_tibble(transformed_data_list[[index]])
+  transformed_data_list <- purr::map(file_paths, read_and_transform)
+  transformed_ <- dplyr::as_tibble(transformed_data_list[[index]])
   return(transformed_)
   # Return a message indicating successful saving
   message("Transformed data saved successfully")
@@ -42,28 +46,32 @@ number_of <- function(state_var, transformed_data) {
     state_var <- as.vector(state_var)
   }
   if (tolower(state_var) == "national") {
-    transformed_data %>%
-      filter(`Strike or Protest` == "Strike") %>%
-      group_by(Year) %>%
-      summarise(
-        `labor org count` = n_distinct(`Labor Organization`,
-          na.rm = TRUE
-        ),
-        employers = n_distinct(`Employer`),
-        strikes = n(),
+    transformed_data |>
+      filter(transformed_data::`Strike or Protest` == "Strike") |>
+      dplyr::group_by(transformed_data::Year) |>
+      dplyr::summarise(
+        `labor org count` =
+          dplyr::n_distinct(transformed_data::`Labor Organization`,
+            na.rm = TRUE
+          ),
+        employers = dplyr::n_distinct(transformed_data::`Employer`),
+        strikes = dplyr::n(),
         .groups = "drop"
       )
   } else {
-    transformed_data %>%
+    transformed_data |>
       filter(
-        State == state_var,
-        `Strike or Protest` == "Strike"
-      ) %>%
-      group_by(State, Year) %>%
-      summarise(
-        `labor org count` = n_distinct(`Labor Organization`, na.rm = TRUE),
-        employers = n_distinct(`Employer`),
-        strikes = n(),
+        transformed_data$State == state_var,
+        transformed_data$`Strike or Protest` == "Strike"
+      ) |>
+      dplyr::group_by(transformed_data$State, transformed_data$Year) |>
+      dplyr::summarise(
+        `labor org count` = dplyr::n_distinct(
+          transformed_data::`Labor Organization`,
+          na.rm = TRUE
+        ),
+        employers = tidyverse::n_distinct(transformed_data::`Employer`),
+        strikes = dplyr::n(),
         .groups = "drop"
       )
   }
@@ -74,29 +82,37 @@ month_year_var_number <- function(state_var, year_var, transformed_data) {
     state_var <- as.vector(state_var)
   }
   if (tolower(state_var) == "national") {
-    transformed_data %>%
+    transformed_data |>
       filter(
-        Year == year_var,
-        `Strike or Protest` == "Strike"
-      ) %>%
-      group_by(Month) %>%
-      summarise(
-        `labor org count` = n_distinct(`Labor Organization`, na.rm = TRUE),
-        employers = n_distinct(`Employer`),
-        strikes = n(),
+        transformed_data::Year == year_var,
+        transformed_data::`Strike or Protest` == "Strike"
+      ) |>
+      dplyr::group_by(transformed_data::Month) |>
+      dplyr::summarise(
+        `labor org count` = tidyverse::n_distinct
+        (transformed_data::`Labor Organization`,
+          na.rm = TRUE
+        ),
+        employers = tidyverse::n_distinct(transformed_data::`Employer`),
+        strikes = dplyr::n(),
         .groups = "drop"
       )
   } else {
-    transformed_data %>%
+    transformed_data |>
       filter(
-        State == state_var, Year == year_var,
-        `Strike or Protest` == "Strike"
-      ) %>%
-      group_by(State, Month) %>%
-      summarise(
-        `labor org count` = n_distinct(`Labor Organization`, na.rm = TRUE),
-        employers = n_distinct(`Employer`),
-        strikes = n(),
+        transformed_data::State == state_var,
+        transformed_data::Year == year_var,
+        transformed_data::`Strike or Protest` == "Strike"
+      ) |>
+      dplyr::group_by(transformed_data::State, transformed_data::Month) |>
+      dplyr::summarise(
+        `labor org count` =
+          tidyverse::n_distinct(
+            transformed_data::`Labor Organization`,
+            na.rm = TRUE
+          ),
+        employers = tidyverse::n_distinct(transformed_data::`Employer`),
+        strikes = dplyr::n(),
         .groups = "drop"
       )
   }
@@ -104,11 +120,11 @@ month_year_var_number <- function(state_var, year_var, transformed_data) {
 
 
 write_data_to_excel <- function(data_list, output_path) {
-  wb <- createWorkbook()
-  map2(names(data_list), data_list, function(name, data) {
-    addWorksheet(wb, name)
-    writeData(wb, name, data)
+  wb <- openxlsx::createWorkbook()
+  purr::map2(names(data_list), data_list, function(name, data) {
+    openxlsx::addWorksheet(wb, name)
+    openenxlsx::writeData(wb, name, data)
   })
-  saveWorkbook(wb, output_path, overwrite = TRUE)
+  openxlsx::saveWorkbook(wb, output_path, overwrite = TRUE)
   return(output_path)
 }
