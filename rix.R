d502d6da13a4fc10b825e226300476b4e4808165
VtheRtech <sakeyisu@gmail.com>
2024-06-06 07:27:15 -0400

    utlized bruno's fix in rix.R

--- rix.R
+++ rix.R
@@ -1,5 +1,12 @@
 library(rix)
 
+setwd("~/Lab4/")
+
+rix_init(
+  project_path = ".",
+  rprofile_action = c("create_missing", "create_backup", "overwrite", "append"),
+  message_type = c("simple", "verbose")
+)
 
 rix(
   r_ver = "latest",
