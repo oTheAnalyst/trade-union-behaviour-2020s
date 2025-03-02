654b5e1d2d6dd45cf0c35bf4e11800113ad6d09f
oTheAnalyst <michaelvcumbo21@proton.me>
2025-03-02 18:46:27 -0500

    started to setup an auto update function for my sqlite database

--- builder.R
+++ builder.R
@@ -1,2 +1,15 @@
+library(lubridate)
+# I'm building a function that find the most recent data and
+# copies it to replaces the prod.csv
+# going to need to have all the data in list
+# apply the date names to that list
+# the data frame thats on an index of 1 is going to be written as prod data
+# and its own datafile respecting the naming covention 
 
-super_function()
+
+files <- base::list.files(here::here("data"))
+files <- base::gsub("Labor-", "", files)
+files <- base::gsub(".xlsx", "", files)
+dates <- files[!grepl(paste0(c("prod", "output"), collapse = "|"), files)]
+desc_dates <- lubridate::mdy(dates) |> sort(decreasing = TRUE)
+print(desc_dates)
