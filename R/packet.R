dfbe9e413ac16d393f58fd4a06efe2ffdbe5260d
oTheAnalyst <michaelvcumbo21@proton.me>
2025-08-09 14:08:11 -0400

    fix colnames, removed nested function, had a single input for the final function call

--- R/packet.R
+++ R/packet.R
@@ -13,6 +13,70 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 #
+
+load_list <- function(input) {
+ #load in the data paths  
+  file_paths <- list.files(
+    path = input,
+    pattern = "\\.xlsx$",
+    full.names = TRUE
+  )
+ 
+#read the path output a list 
+ files <- file_paths |> 
+  purrr::map(readxl::read_xlsx) 
+ # set the name as a list
+# names <-  basename(file_paths )
+# names <- gsub(".xlsx", "", names)
+#  files <- files |> 
+#   purrr::set_names(names) 
+ return(files)
+}
+
+transform_data <- function(input) {
+  # Read Excel file and convert to tibble
+  excel_tibble <- input
+  
+  excel_tibble$s_action <- as.POSIXct(
+        excel_tibble$'Start Date',
+        format = "%m/%d/%Y", tz = "UTC"
+        )
+  excel_tibble$Year <- lubridate::year(excel_tibble$s_action)
+  excel_tibble$Month <- lubridate::month(excel_tibble$s_action)
+  tx_data <- excel_tibble
+  # Mutate columns as needed
+#  tx_data <- excel_tibble |>
+#    dplyr::mutate(
+#      ZipCode = as.character('Zip Code'),
+#      BargainingUnitSize =
+#        readr::parse_number(as.character('Bargaining Unit Size')),
+#      ApproximateNumberofParticipants =
+#        readr::parse_number(as.character('Approximate Number of Participants')),
+#      Date = format(s_action, "%m-%d-%Y"),
+#      month = format(s_action, "%B"),
+#      DurationAmount = as.integer('Duration Amount')
+#    )
+# 
+  tx_data <- tx_data |>
+    janitor::clean_names(case = "snake")
+  
+  return(tx_data)
+}
+
+load_transform_data <- function(input) {
+  #input list
+  input_list <- load_list(input)
+  #this transforms the the data
+  tx_list <- purrr::map(input_list, transform_data)
+  
+  return(tx_list)
+  # Return a message indicating successful saving
+  message("Transformed data saved successfully")
+}
+
+load_transform_data(here::here("data"))
+
+
 write_to_sql <- function(data, name) {
   driver <- RSQLite::dbDriver("SQLite")
   sql_location <- "~/trade_union-strikes.db"
@@ -20,60 +84,5 @@ write_to_sql <- function(data, name) {
   RSQLite::dbWriteTable(conn, name, data, overwrite = TRUE)
   RSQLite::dbDisconnect(conn)
 }
-
-
-transform_data <- function(input_list) {
-  # Read Excel file and convert to tibble
-  excel_tibble <- readxl::read_excel(input_list)
-  excel_tibble$Timestamp <- as.POSIXct(excel_tibble$'Start Date',
-    format = "%m/%d/%Y", tz = "UTC"
-  )
-  excel_tibble$Year <- lubridate::year(excel_tibble$Timestamp)
-  excel_tibble$Month <- lubridate::month(excel_tibble$Timestamp)
-  return(excel_tibble)
-  # Mutate columns as needed
-  transformed_data <- excel_tibble |>
-    dplyr::mutate(
-      ZipCode = as.character('Zip Code'),
-      BargainingUnitSize =
-        readr::parse_number(as.character('Bargaining Unit Size')),
-      ApproximateNumberofParticipants =
-        readr::parse_number(as.character('Approximate Number of Participants')),
-      Date = format(Timestamp, "%m-%d-%Y"),
-      month = format(Timestamp, "%B"),
-      DurationAmount = as.integer('Duration Amount')
-    )
-  return(transformed_data)
-}
-  
-  
-tx_load_list <- function() {
-  file_paths <- list.files(
-    path = here::here("data"),
-    pattern = "\\.xlsx$",
-    full.names = TRUE
-  )
-  return(file_paths)
-}
-
-
-load_and_transform_data <- function(index) {
-  input_list <- tx_load_list()
-  # Get a list of all xlsx files in the specified directory
-  print(input_list)
-  # Function to read Excel file, convert to tibble, and transform data
-  # Read all Excel files, store them as tibbles, and transform data
-  names <- base::basename(input_list)
-  transformed_data_list <- purrr::map(input_list, transform_data)
-  transformed_data_list <- transformed_data_list |> purrr::set_names(names)
-  transformed_ <- tibble::as_tibble(transformed_data_list[[index]])
-  return(transformed_)
-  # Return a message indicating successful saving
-  message("Transformed data saved successfully")
-}
-
-
-load_and_transform_data("Labor-prod.xlsx")
-
 
 
