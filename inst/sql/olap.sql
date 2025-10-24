f638aa3ba834942d6c12cc3821ac9fdd4cb80271
oTheAnalyst <michaelvcumbo21@proton.me>
2025-10-24 10:47:53 -0400

    added temp table to olap simplified the queries some what

--- inst/sql/olap.sql
+++ inst/sql/olap.sql
@@ -1,173 +1,41 @@
+--SET temp_directory = '~/temp_dir/';
+--drop table sop
+CREATE TEMP TABLE sop AS
+    SELECT
+    d.*,
+    s.*,
+    e.*
+    FROM production.main.date_key d
+    left join production.main.strikeOrProtest s
+    on d.id = s.id 
+    left join production.main.location e
+    on e.id = d.id 
+    where s.id is not null
+;
+
+
 -- past 5 year protest
 SELECT
 distinct
-count(s.*) as NumberofProtestInMarylandInThePast5years
-FROM production.main.date_key d
-left join production.main.strikeOrProtest s
-on d.id = s.id 
-left join production.main.location e 
-on e.id = d.id 
-where s.id is not null
-and s.strikeOrProtest ILIKE 'protest'
-and e.state ILIKE 'maryland'
-;
-
-
-PREPARE protestyear AS 
-SELECT
-distinct
-count(s.*) as numberofprotestinayear,
-monthname(s.startDate) as monthname,
-month(s.startDate) numMonth
-FROM production.main.date_key d
-left join production.main.strikeOrProtest s
-on d.id = s.id
-left join production.main.location e
-on e.id = d.id
-where s.id is not null
-and s.strikeOrProtest ILIKE $strikeorprotest 
-and e.state ILIKE 'maryland'
-and s.startDate in(
-    SELECT
-    startDate
-    FROM production.main.strikeOrProtest
-    WHERE year(startDate) = $year
-)
-GROUP BY monthname, numMonth
-ORDER BY numMonth ASC
-;
-
-
-EXECUTE protestyear(strikeorprotest := 'protest', year := 2021);
-
-SELECT
-distinct
-count(s.*) as numberofprotest2022,
-monthname(s.startDate) as monthname,
-month(s.startDate) numMonth
-FROM production.main.date_key d
-left join production.main.strikeOrProtest s
-on d.id = s.id
-left join production.main.location e
-on e.id = d.id
-where s.id is not null
-and s.strikeOrProtest ILIKE 'protest'
-and e.state ILIKE 'maryland'
-and s.startDate in(
-    SELECT
-    startDate
-    FROM production.main.strikeOrProtest
-    WHERE year(startDate) = 2022
-)
-GROUP BY monthname, numMonth
-ORDER BY numMonth ASC
-;
-
-
--- strikes in maryland 
-SELECT
-distinct
-count(s.*) as numberofprotest2023,
-monthname(s.startDate) as monthname,
-month(s.startDate) numMonth
-FROM production.main.date_key d
-left join production.main.strikeOrProtest s
-on d.id = s.id
-left join production.main.location e
-on e.id = d.id
-where s.id is not null
-and s.strikeOrProtest ILIKE 'protest'
-and e.state ILIKE 'maryland'
-and s.startDate in(
-    SELECT
-    startDate
-    FROM production.main.strikeOrProtest
-    WHERE year(startDate) = 2023
-)
-GROUP BY monthname, numMonth
-ORDER BY numMonth ASC
+count(*) as NumberofProtestIn_maryland_InThePast5years
+FROM
+sop
+where
+strikeOrProtest ILIKE 'protest'
+and state ILIKE 'maryland'
 ;
 
 -- strikes in maryland 
 SELECT
-distinct
-count(s.*) as numberofprotest2024,
-monthname(s.startDate) as monthname,
-month(s.startDate) numMonth
-FROM production.main.date_key d
-left join production.main.strikeOrProtest s
-on d.id = s.id
-left join production.main.location e
-on e.id = d.id
-where s.id is not null
-and s.strikeOrProtest ILIKE 'protest'
-and e.state ILIKE 'maryland'
-and s.startDate in(
-    SELECT
-    startDate
-    FROM production.main.strikeOrProtest
-    WHERE year(startDate) = 2024
-)
-GROUP BY monthname, numMonth
-ORDER BY numMonth ASC
-;
-
-SELECT
-distinct
-count(s.*) as numberofprotest2025,
-monthname(s.startDate) as monthname,
-month(s.startDate) numMonth
-FROM production.main.date_key d
-left join production.main.strikeOrProtest s
-on d.id = s.id
-left join production.main.location e
-on e.id = d.id
-where s.id is not null
-and s.strikeOrProtest ILIKE 'protest'
-and e.state ILIKE 'maryland'
-and s.startDate in(
-    SELECT
-    startDate
-    FROM production.main.strikeOrProtest
-    WHERE year(startDate) = 2025
-)
-GROUP BY monthname, numMonth
-ORDER BY numMonth ASC
-;
-
-
-
-
--- past 5 year strike
-SELECT
-distinct
-count(s.*) as NumberofstrikestInMarylandInThePast5years
-FROM production.main.date_key d
-left join production.main.strikeOrProtest s
-on d.id = s.id 
-left join production.main.location e 
-on e.id = d.id 
-where s.id is not null
-and s.strikeOrProtest ILIKE 'strike'
-and e.state ILIKE 'maryland'
-;
-
-
--- strikes in maryland 
-SELECT
-distinct
-count(s.*) as numberofstrikes2021,
-monthname(s.startDate) as monthname,
-month(s.startDate) numMonth
-FROM production.main.date_key d
-left join production.main.strikeOrProtest s
-on d.id = s.id
-left join production.main.location e
-on e.id = d.id
-where s.id is not null
-and s.strikeOrProtest ILIKE 'strike'
-and e.state ILIKE 'maryland'
-and s.startDate in(
+DISTINCT
+count(*) as numberofprotest2021,
+monthname(startDate) as monthname,
+month(startDate) numMonth
+FROM sop
+WHERE
+strikeOrProtest ILIKE 'protest'
+and state ILIKE 'maryland'
+and startDate in(
     SELECT
     startDate
     FROM production.main.strikeOrProtest
@@ -178,21 +46,16 @@ ORDER BY numMonth ASC
 ;
 
 
--- strikes in maryland 
 SELECT
-distinct
-count(s.*) as numberofstrikes2022,
-monthname(s.startDate) as monthname,
-month(s.startDate) numMonth
-FROM production.main.date_key d
-left join production.main.strikeOrProtest s
-on d.id = s.id
-left join production.main.location e
-on e.id = d.id
-where s.id is not null
-and s.strikeOrProtest ILIKE 'strike'
-and e.state ILIKE 'maryland'
-and s.startDate in(
+DISTINCT
+count(*) as numberofprotest2022,
+monthname(startDate) as monthname,
+month(startDate) numMonth
+FROM sop
+WHERE
+strikeOrProtest ILIKE 'protest'
+and state ILIKE 'maryland'
+and startDate in(
     SELECT
     startDate
     FROM production.main.strikeOrProtest
@@ -202,21 +65,16 @@ GROUP BY monthname, numMonth
 ORDER BY numMonth ASC
 ;
 
--- strikes in maryland 
 SELECT
-distinct
-count(s.*) as numberofstrikes2023,
-monthname(s.startDate) as monthname,
-month(s.startDate) numMonth
-FROM production.main.date_key d
-left join production.main.strikeOrProtest s
-on d.id = s.id
-left join production.main.location e
-on e.id = d.id
-where s.id is not null
-and s.strikeOrProtest ILIKE 'strike'
-and e.state ILIKE 'maryland'
-and s.startDate in(
+DISTINCT
+count(*) as numberofprotest2023,
+monthname(startDate) as monthname,
+month(startDate) numMonth
+FROM sop
+WHERE
+strikeOrProtest ILIKE 'protest'
+and state ILIKE 'maryland'
+and startDate in(
     SELECT
     startDate
     FROM production.main.strikeOrProtest
@@ -226,21 +84,16 @@ GROUP BY monthname, numMonth
 ORDER BY numMonth ASC
 ;
 
--- strikes in maryland 
 SELECT
-distinct
-count(s.*) as numberofstrikes2024,
-monthname(s.startDate) as monthname,
-month(s.startDate) numMonth
-FROM production.main.date_key d
-left join production.main.strikeOrProtest s
-on d.id = s.id
-left join production.main.location e
-on e.id = d.id
-where s.id is not null
-and s.strikeOrProtest ILIKE 'strike'
-and e.state ILIKE 'maryland'
-and s.startDate in(
+DISTINCT
+count(*) as numberofprotest2024,
+monthname(startDate) as monthname,
+month(startDate) numMonth
+FROM sop
+WHERE
+strikeOrProtest ILIKE 'protest'
+and state ILIKE 'maryland'
+and startDate in(
     SELECT
     startDate
     FROM production.main.strikeOrProtest
@@ -251,19 +104,15 @@ ORDER BY numMonth ASC
 ;
 
 SELECT
-distinct
-count(s.*) as numberofstrikes2025,
-monthname(s.startDate) as monthname,
-month(s.startDate) numMonth
-FROM production.main.date_key d
-left join production.main.strikeOrProtest s
-on d.id = s.id
-left join production.main.location e
-on e.id = d.id
-where s.id is not null
-and s.strikeOrProtest ILIKE 'strike'
-and e.state ILIKE 'maryland'
-and s.startDate in(
+DISTINCT
+count(*) as numberofprotest2025,
+monthname(startDate) as monthname,
+month(startDate) numMonth
+FROM sop
+WHERE
+strikeOrProtest ILIKE 'protest'
+and state ILIKE 'maryland'
+and startDate in(
     SELECT
     startDate
     FROM production.main.strikeOrProtest
@@ -274,29 +123,112 @@ ORDER BY numMonth ASC
 ;
 
 
--- Most active trade_unions in the country
+
+-- past 5 year strike
 SELECT
-count(t.laborOrganization) as Union_Strike_Leaderboard,
-t.laborOrganization
-FROM production.main.date_key d
-LEFT JOIN production.main.strikeOrProtest s
-ON d.id = s.id
-LEFT JOIN production.main.location e
-ON e.id = d.id
-LEFT JOIN production.main.trade_union t
-ON d.id = t.id
+distinct
+count(*) as Numberof_strikes_In_maryland_InThePast5years
+FROM
+sop
+where
+strikeOrProtest ILIKE 'strike'
+and state ILIKE 'maryland'
+;
+
+
+SELECT
+DISTINCT
+count(*) as numberofstrike2021,
+monthname(startDate) as monthname,
+month(startDate) numMonth
+FROM sop
 WHERE
-s.strikeOrProtest ILIKE 'strike'
-AND t.laborOrganization is not null
-AND s.startDate
-IN(
+strikeOrProtest ILIKE 'strike'
+and state ILIKE 'maryland'
+and startDate in(
+    SELECT
+    startDate
+    FROM production.main.strikeOrProtest
+    WHERE year(startDate) = 2021
+)
+GROUP BY monthname, numMonth
+ORDER BY numMonth ASC
+;
+
+
+SELECT
+DISTINCT
+count(*) as numberofstrike2023,
+monthname(startDate) as monthname,
+month(startDate) numMonth
+FROM sop
+WHERE
+strikeOrProtest ILIKE 'strike'
+and state ILIKE 'maryland'
+and startDate in(
+    SELECT
+    startDate
+    FROM production.main.strikeOrProtest
+    WHERE year(startDate) = 2022
+)
+GROUP BY monthname, numMonth
+ORDER BY numMonth ASC
+;
+
+SELECT
+DISTINCT
+count(*) as numberofstrike2023,
+monthname(startDate) as monthname,
+month(startDate) numMonth
+FROM sop
+WHERE
+strikeOrProtest ILIKE 'strike'
+and state ILIKE 'maryland'
+and startDate in(
+    SELECT
+    startDate
+    FROM production.main.strikeOrProtest
+    WHERE year(startDate) = 2023
+)
+GROUP BY monthname, numMonth
+ORDER BY numMonth ASC
+;
+
+SELECT
+DISTINCT
+count(*) as numberofstrike2024,
+monthname(startDate) as monthname,
+month(startDate) numMonth
+FROM sop
+WHERE
+strikeOrProtest ILIKE 'strike'
+and state ILIKE 'maryland'
+and startDate in(
+    SELECT
+    startDate
+    FROM production.main.strikeOrProtest
+    WHERE year(startDate) = 2024
+)
+GROUP BY monthname, numMonth
+ORDER BY numMonth ASC
+;
+
+SELECT
+DISTINCT
+count(*) as numberofstrike2025,
+monthname(startDate) as monthname,
+month(startDate) numMonth
+FROM sop
+WHERE
+strikeOrProtest ILIKE 'strike'
+and state ILIKE 'maryland'
+and startDate in(
     SELECT
     startDate
     FROM production.main.strikeOrProtest
     WHERE year(startDate) = 2025
 )
-GROUP BY t.laborOrganization
-ORDER BY Union_Strike_Leaderboard DESC
+GROUP BY monthname, numMonth
+ORDER BY numMonth ASC
 ;
-
 
