c81f4bdc90d04fbeef539b924d5c1519741b74ef
VtheRtech <michaelvcumbo21@proton.me>
2026-02-03 22:07:31 -0500

    added spatial table and geom ingestion

--- inst/sql/inserts/insert_main.sql
+++ inst/sql/inserts/insert_main.sql
@@ -84,6 +84,24 @@ select id from main.lat_lon
 )
 ;
 
+LOAD spatial;
+INSERT INTO main.geom
+select  
+  l1.id,
+  st_point(
+  split_part(STRING_SPLIT(l1.latitudeLongitude, ';').UNNEST(), ',',2)::double,
+  split_part(STRING_SPLIT(l2.latitudeLongitude, ';').UNNEST(), ',', 1)::double
+  ) as point
+from stg_lat as l1
+join stg_lat as l2
+on l1.id = l2.id
+WHERE
+l1.id NOT IN(
+select id from main.geom
+);
+
+
+
 
 INSERT INTO main.location
 select
