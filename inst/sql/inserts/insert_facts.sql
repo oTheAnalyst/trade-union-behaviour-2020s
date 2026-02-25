7601c8958f11cb7816959933a14246258a995ab2
VtheRtech <michaelvcumbo21@proton.me>
2026-02-25 12:29:25 -0500

    added star inserts

--- inst/sql/inserts/insert_facts.sql
+++ inst/sql/inserts/insert_facts.sql
@@ -1,14 +1,20 @@
-insert into fact_lat 
+insert into strike_fact 
 select 
-  "strikeOrProtest".internal_id as strikeOrProtest_id,
-  trade_union.internal_id as trade_uion_id,
-  employer.internal_id as employer_id,
-  "location".internal_id
+  "strikeOrProtest".internal_id as strike_or_protest_id,
+  "location".internal_id as location_id,
+  tradeUnion.internal_id as trade_union_id,
+  workerDemands.internal_id as workerDemands_id,
+  citations.internal_id as citations_id,
+  employer.internal_id as employer_id
 from "strikeOrProtest"
-join trade_union
-on "strikeOrProtest".id = trade_union.id
+join tradeUnion
+on "strikeOrProtest".id = tradeUnion.id
 join employer
 on "strikeOrProtest".id = employer.id
 join "location" 
 on "strikeOrProtest".id = "location".id
+join workerDemands
+on "strikeOrProtest".id = "workerDemands".id
+join citations
+on "strikeOrProtest".id = "citations".id
 order by "strikeOrProtest".id desc
--- inst/sql/inserts/insert_main.sql
+++ inst/sql/inserts/insert_main.sql
@@ -1,6 +1,8 @@
 INSERT INTO main.strikeOrProtest
 select
+nextval('strikeOrProtest_id'),
 id,
+STRING_SPLIT(workerDemands,';').UNNEST(),
 approximateNumberOfParticipants,
 startDate,
 endDate,
@@ -19,27 +21,17 @@ select id from main.strikeOrProtest
 
 
 
-INSERT INTO main.workerDemands 
-select
-id,
-STRING_SPLIT(workerDemands,';').UNNEST() as t1
-from stg_lat
-WHERE 
-id NOT IN(
-select id from main.workerDemands
-)
-;
 
-
-INSERT INTO main.trade_union 
+INSERT INTO main.tradeUnion 
 select
+nextval('trade_union_id'),
 id,
 STRING_SPLIT(laborOrganization,';').UNNEST() as t2,
 bargainingUnitSize
 from stg_lat
 WHERE 
 id NOT IN(
-select id from main.trade_union
+select id from main.tradeUnion
 )
 ;
 
@@ -47,9 +39,10 @@ select id from main.trade_union
 
 INSERT INTO main.citations
 select
+nextval('citation_id'),
 id,
 STRING_SPLIT(source,';').UNNEST() as s,
-notes,
+notes
 from stg_lat
 WHERE startDate IS NOT NULL
 and
@@ -61,6 +54,7 @@ select id from main.citations
 
 INSERT INTO main.employer
 select
+nextval('employer_id'),
 id,
 STRING_SPLIT(local, ';').UNNEST() as local,
 STRING_SPLIT(industry, ';').UNNEST() as industry,
@@ -73,21 +67,16 @@ select id from main.employer
 
 ;
 
-INSERT INTO main.lat_lon
-select 
-id,
-STRING_SPLIT(latitudeLongitude, ';').UNNEST() latitudeLongitude
-from stg_lat
-WHERE
-id NOT IN(
-select id from main.lat_lon
-)
-;
 
 LOAD spatial;
-INSERT INTO main.geom
-select  
-  l1.id,
+INSERT INTO main.location
+select
+nextval('location_id'),
+l1.id,
+STRING_SPLIT(l1.state, ';').UNNEST() State,
+STRING_SPLIT(l1.address, ';').UNNEST() Address,
+STRING_SPLIT(l1.city, ';').UNNEST() City,
+STRING_SPLIT(l1.zipCode, ';').UNNEST() zipcode,
   st_point(
   split_part(STRING_SPLIT(l1.latitudeLongitude, ';').UNNEST(), ',',2)::double,
   split_part(STRING_SPLIT(l2.latitudeLongitude, ';').UNNEST(), ',', 1)::double
@@ -97,22 +86,6 @@ join stg_lat as l2
 on l1.id = l2.id
 WHERE
 l1.id NOT IN(
-select id from main.geom
-);
-
-
-
-
-INSERT INTO main.location
-select
-id,
-STRING_SPLIT(state, ';').UNNEST() State,
-STRING_SPLIT(address, ';').UNNEST() Address,
-STRING_SPLIT(city, ';').UNNEST() City,
-STRING_SPLIT(zipCode, ';').UNNEST() zipcode
-from stg_lat
-WHERE
-id NOT IN(
 select id from main.location
 )
 ;
